174419955b737f7ba775afd504da9d7ebacb3bfd
gnanodhay randhi <randhignanodhay@gmail.com>
2025-12-10 00:18:41 +0530

    Second comit

--- src/main/java/com/gnanu/quizapp/Employee.java
+++ src/main/java/com/gnanu/quizapp/Employee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-
+// ashbel is a good boy
 @Entity
 public class Employee {
 	 	@Id
